214c14dad2d81dc1eb616c073d4b55e19a4216b6
arboundy <arboundy@gmail.com>
2018-04-15 23:17:30 +0930

    1

--- UnifiedAuditLog-To-Sql.ps1
+++ UnifiedAuditLog-To-Sql.ps1
@@ -1,5 +1,5 @@
 
-<#
+#<#
 $Credential = Get-Credential
 $ExchangeSession = New-PSSession `
     -ConfigurationName Microsoft.Exchange `
@@ -16,12 +16,13 @@ $results = Search-UnifiedAuditLog -StartDate $start_date -EndDate $end_date -Ses
 $results | Out-File result.txt
 
 $oUnifiedAuditLogs = @()
-$oUnifiedAuditLogExchangeItems = @()
-$oUnifiedAuditLogAzureActiveDirectoryStsLogons = @()
+$oExchangeItems = @()
+$oAzureActiveDirectoryStsLogons = @()
+$oPowerBIAudits = @()
 
 foreach ($result in $results) {
     $oUnifiedAuditLog = New-Object –TypeName PSObject
-    $oUnifiedAuditLog | Add-Member –MemberType NoteProperty –Name Identity –Value $result.Identity
+    $oUnifiedAuditLog | Add-Member –MemberType NoteProperty –Name UnifiedAuditLogIdentity –Value $result.Identity
     $oUnifiedAuditLog | Add-Member –MemberType NoteProperty –Name RunspaceId –Value $result.RunspaceId
     $oUnifiedAuditLog | Add-Member –MemberType NoteProperty –Name RecordType –Value $result.RecordType
     $oUnifiedAuditLog | Add-Member –MemberType NoteProperty –Name CreationDate –Value $result.CreationDate
@@ -36,7 +37,7 @@ foreach ($result in $results) {
 
     if($result.RecordType -eq 'ExchangeItem') {
         $oExchangeItem = New-Object –TypeName PSObject
-        $oExchangeItem | Add-Member –MemberType NoteProperty –Name Identity –Value $result.Identity
+        $oExchangeItem | Add-Member –MemberType NoteProperty –Name UnifiedAuditLogIdentity –Value $result.Identity
         $oExchangeItem | Add-Member –MemberType NoteProperty –Name CreationTime –Value $oAuditData.CreationTime
         $oExchangeItem | Add-Member –MemberType NoteProperty –Name Id –Value $oAuditData.Id
         $oExchangeItem | Add-Member –MemberType NoteProperty –Name Operation –Value $oAuditData.Operation
@@ -54,11 +55,12 @@ foreach ($result in $results) {
         $oExchangeItem | Add-Member –MemberType NoteProperty –Name MailboxOwnerUPN –Value $oAuditData.MailboxOwnerUPN
         $oExchangeItem | Add-Member –MemberType NoteProperty –Name OrganziationalName –Value $oAuditData.OrganziationalName
         $oExchangeItem | Add-Member –MemberType NoteProperty –Name OriginatingServer –Value $oAuditData.OriginatingServer
-        $oUnifiedAuditLogExchangeItems += $oExchangeItem
+        $oExchangeItems += $oExchangeItem
     }    
 
     if($result.RecordType -eq 'AzureActiveDirectoryStsLogon') {
         $oAzureActiveDirectoryStsLogon = New-Object –TypeName PSObject
+        $oExchangeItem | Add-Member –MemberType NoteProperty –Name UnifiedAuditLogIdentity –Value $result.Identity
         $oAzureActiveDirectoryStsLogon | Add-Member –MemberType NoteProperty –Name CreationTime –Value $oAuditData.CreationTime
         $oAzureActiveDirectoryStsLogon | Add-Member –MemberType NoteProperty –Name Id –Value $oAuditData.Id
         $oAzureActiveDirectoryStsLogon | Add-Member –MemberType NoteProperty –Name Operation –Value $oAuditData.Operation
@@ -82,12 +84,41 @@ foreach ($result in $results) {
         $oAzureActiveDirectoryStsLogon | Add-Member –MemberType NoteProperty –Name Target –Value $oAuditData.Target
         $oAzureActiveDirectoryStsLogon | Add-Member –MemberType NoteProperty –Name TargetContextId –Value $oAuditData.TargetContextId
         $oAzureActiveDirectoryStsLogon | Add-Member –MemberType NoteProperty –Name ApplicationId –Value $oAuditData.ApplicationId
-        $oUnifiedAuditLogAzureActiveDirectoryStsLogons += $oAzureActiveDirectoryStsLogon
+        $oAzureActiveDirectoryStsLogons += $oAzureActiveDirectoryStsLogon
+    }
+
+    if($result.RecordType -eq 'PowerBIAudit') {
+        $oPowerBIAudit = New-Object –TypeName PSObject
+        $oPowerBIAudit | Add-Member –MemberType NoteProperty –Name UnifiedAuditLogIdentity –Value $result.Identity
+        $oPowerBIAudit | Add-Member –MemberType NoteProperty –Name Id –Value $oAuditData.Id
+        $oPowerBIAudit | Add-Member –MemberType NoteProperty –Name RecordType –Value $oAuditData.RecordType
+        $oPowerBIAudit | Add-Member –MemberType NoteProperty –Name CreationTime –Value $oAuditData.CreationTime
+        $oPowerBIAudit | Add-Member –MemberType NoteProperty –Name Operation –Value $oAuditData.Operation
+        $oPowerBIAudit | Add-Member –MemberType NoteProperty –Name OrganizationId –Value $oAuditData.OrganizationId
+        $oPowerBIAudit | Add-Member –MemberType NoteProperty –Name UserKey –Value $oAuditData.UserKey
+        $oPowerBIAudit | Add-Member –MemberType NoteProperty –Name UserType –Value $oAuditData.UserType
+        $oPowerBIAudit | Add-Member –MemberType NoteProperty –Name Workload –Value $oAuditData.Workload
+        $oPowerBIAudit | Add-Member –MemberType NoteProperty –Name UserId –Value $oAuditData.UserId
+        $oPowerBIAudit | Add-Member –MemberType NoteProperty –Name ClientIP –Value $oAuditData.ClientIP
+        $oPowerBIAudit | Add-Member –MemberType NoteProperty –Name UserAgent –Value $oAuditData.UserAgent
+        $oPowerBIAudit | Add-Member –MemberType NoteProperty –Name Activity –Value $oAuditData.Activity
+        $oPowerBIAudit | Add-Member –MemberType NoteProperty –Name ItemName –Value $oAuditData.ItemName
+        $oPowerBIAudit | Add-Member –MemberType NoteProperty –Name WorkSpaceName –Value $oAuditData.WorkSpaceName
+        $oPowerBIAudit | Add-Member –MemberType NoteProperty –Name DatasetName –Value $oAuditData.DatasetName
+        $oPowerBIAudit | Add-Member –MemberType NoteProperty –Name ReportName –Value $oAuditData.ReportName
+        $oPowerBIAudit | Add-Member –MemberType NoteProperty –Name WorkspaceId –Value $oAuditData.WorkspaceId
+        $oPowerBIAudit | Add-Member –MemberType NoteProperty –Name ObjectId –Value $oAuditData.ObjectId
+        $oPowerBIAudit | Add-Member –MemberType NoteProperty –Name DatasetId –Value $oAuditData.DatasetId
+        $oPowerBIAudit | Add-Member –MemberType NoteProperty –Name ReportId –Value $oAuditData.ReportId
+        $oPowerBIAudit | Add-Member –MemberType NoteProperty –Name IsSuccess –Value $oAuditData.IsSuccess
+        $oPowerBIAudits += $oPowerBIAudit
     }
 }
 $oUnifiedAuditLogs | Export-Csv -NoTypeInformation "oUnifiedAuditLogs-$filestamp.csv"
-$oUnifiedAuditLogExchangeItems | Export-Csv -NoTypeInformation "oUnifiedAuditLogs-ExchangeItems-$filestamp.csv"
-$oUnifiedAuditLogAzureActiveDirectoryStsLogons | Export-Csv -NoTypeInformation "oUnifiedAuditLogs-AzureActiveDirectoryStsLogons-$filestamp.csv"
+$oExchangeItems | Export-Csv -NoTypeInformation "oExchangeItems-$filestamp.csv"
+$oAzureActiveDirectoryStsLogons | Export-Csv -NoTypeInformation "oAzureActiveDirectoryStsLogons-$filestamp.csv"
+$oPowerBIAudits | Export-Csv -NoTypeInformation "oPowerBIAudits-$filestamp.csv"
+
 
 <#
 AzureActiveDirectory
